ef7cd97c11a0f187118bb983826dc6ff77f1b2cb
MrBlubke <ward@impesoft.com>
2020-12-18 19:25:52 +0100

    defining XML

--- WardsPizzeria/Program.cs
+++ WardsPizzeria/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace WardsPizzeria
 {
@@ -6,7 +8,23 @@ namespace WardsPizzeria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee bs = new Employee();
+
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+
+            TextWriter txtWriter = new StreamWriter(@ "D:\Serialization.xml");
+
+            xs.Serialize(txtWriter, bs);
+
+            txtWriter.Close();
+
         }
     }
+    public class Employee
+    {
+        public int Id = 1;
+        public String name = "John Smith";
+        public string subject = "Physics";
+    }
+
 }
