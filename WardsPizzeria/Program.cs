724f909d7b53ec29bb2bd59c4c33347add44af76
MrBlubke <ward@impesoft.com>
2020-12-18 19:23:30 +0100

    Add project files.

---
+++ WardsPizzeria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WardsPizzeria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
