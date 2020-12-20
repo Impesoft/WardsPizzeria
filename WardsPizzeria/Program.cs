f11624897a2249f6d61ea3453d2e60b31130870e
MrBlubke <ward@impesoft.com>
2020-12-20 21:38:16 +0100

    first question added

--- WardsPizzeria/Program.cs
+++ WardsPizzeria/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
+using System.Text;
 
 namespace WardsPizzeria
 {
@@ -13,7 +14,10 @@ namespace WardsPizzeria
 
         private static void Main(string[] args)
         {
-             //pizzaList.Add(new Pizza(1, "Margherita", "Kaas, tomatensaus, kruiden", true, 5.0));
+            //Console.SetWindowSize(120, 20);
+            Console.Title = "Pizza App(Admin)";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //pizzaList.Add(new Pizza(1, "Margherita", "Kaas, tomatensaus, kruiden", true, 5.0));
 
             Pizza pizza = new Pizza();
             // pizza.WritePizzasToFile(path);
@@ -22,15 +26,25 @@ namespace WardsPizzeria
                 pizza.ReadPizzasFromFile(path);
                 foreach (Pizza loadedpizza in pizza.PizzaList)
                 {
-                    Console.Write($"Pizza {loadedpizza.Name} ingredients(");
-                    Console.Write($"{loadedpizza.PizzaIngredients}) price € ");
+                    Console.Write($"Pizza {loadedpizza.Name}");
+                    Console.CursorLeft = 25;
+                    Console.Write($"ingredients({loadedpizza.PizzaIngredients}) price € ");
                     Console.Write($"{(double)loadedpizza.OrderPrice} veggie:");
                     Console.WriteLine(loadedpizza.IsVeggie);
                 }
             } else
             {
-                Console.WriteLine("Currently we don't have any pizza's available for sale...\nDo you want to add some pizza's to the pizzalist?");
-                Console.ReadLine();
+                Console.WriteLine("Currently we don't have any pizza's available for sale...\nDo you want to add some pizza's to the pizzalist?(Y/N)");
+                char yesNo = char.ToUpper(Convert.ToChar(Console.ReadKey()));
+                switch (yesNo)
+                {
+                    case 'Y':
+                        // pizzamaker
+                        break;
+                    case 'N':
+                        // main menu
+                        break;
+                }
             }
         }
 
